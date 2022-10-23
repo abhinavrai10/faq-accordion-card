d25b4e44c3346fdc785ad80845b1158ca8537fb3
Abhinav <rai.akr10@gmail.com>
2022-10-23 11:07:22 +0530

    next-config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const { mainModule } = require('process')
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   images: {
     loader: "akamai",
     path: "",
