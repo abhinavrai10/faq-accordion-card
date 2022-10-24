6651f55967f602596f24baacf0ba49a642c59059
Abhinav <rai.akr10@gmail.com>
2022-10-24 10:02:46 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-// const { mainModule } = require("process");
-// const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
   images: {
     loader: "akamai",
     path: '',
   },
-  // // basePath: "/nextjs-pages",
-  // assetPrefix: "/nextjs-pages",
 };
 module.exports = nextConfig;
